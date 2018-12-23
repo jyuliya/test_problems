6c709c3a1810910a2f00b9fc6687c5431fbd2af2
Yulia <jyuliya_reneva@mail.ru>
2018-12-23 19:17:50 +0300

    Improvings

--- task_2/whois.sh
+++ task_2/whois.sh
@@ -4,16 +4,21 @@ type=$2
 domen=$3
 url="https://www.whois.com/whois/"
 
+if [ ! -d "/$source_path" ]; then  mkdir "$source_path"; fi
 
-if [[ "$type" -eq "domain" ]]
-then
-echo "$source_path/$type.html"
-curl -o "$source_path/$type.html" "$url$domen"
-fi
-
-if [[ "$type" -eq "file" ]]
+if [[ "$type" == "file" ]]
 then
 while read LINE
-    do curl -o "$source_path/$type.html" "$url$LINE"
-done < "$domen"
+    do  name=(${LINE//./_})
+    	if [ ! -f "/$source_path/$name.html" ]; then  cat >  "/$source_path/$name.html"; fi
+    	curl -o "$source_path/$name.html" "$url$LINE"
+	done < "$domen"
 fi
+
+if [[ "$type" == "domain" ]]
+then
+name=(${domen//./_})
+if ! test -f "$source_path/$name.html"; then  cat > "$source_path/$name.html"; fi
+curl -o "$source_path/$name.html" "$url$domen"
+fi
+
